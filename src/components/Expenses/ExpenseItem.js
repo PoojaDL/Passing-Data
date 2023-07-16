25d100a1e0ab23a1710a01a97a1acb8f45b3a125
PoojaDL <poojadl2002@gmail.com>
2023-07-16 22:13:28 +0530

    Added delete functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,12 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  function deleteExpense(target) {
+    const expenseItem = target.parentElement.parentElement;
+    const expense = document.querySelector(".expenses");
+    expense.removeChild(expenseItem);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <h4>{props.locationOfExpenditure}</h4>
         <div className="expense-item__price">₹{props.amount}</div>
+        <button onClick={(e) => deleteExpense(e.target)}>Delete</button>
       </div>
     </Card>
   );
