abcccef54d242be571633c4dba7dda033944ca35
PoojaDL <poojadl2002@gmail.com>
2023-07-17 20:31:15 +0530

    Updated price functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  function deleteExpense(target) {
-    const expenseItem = target.parentElement.parentElement;
-    const expense = document.querySelector(".expenses");
-    expense.removeChild(expenseItem);
+  const [amount, setAmount] = useState(props.amount);
+  function clickHandler() {
+    console.log(amount);
+    setAmount("$100");
   }
 
   return (
@@ -15,8 +16,8 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <h4>{props.locationOfExpenditure}</h4>
-        <div className="expense-item__price">₹{props.amount}</div>
-        <button onClick={(e) => deleteExpense(e.target)}>Delete</button>
+        <div className="expense-item__price">₹{amount}</div>
+        <button onClick={clickHandler}>update</button>
       </div>
     </Card>
   );
