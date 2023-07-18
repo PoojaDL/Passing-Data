a3e89b40b7abbae1e52874d51e7f362c8504d45a
PoojaDL <poojadl2002@gmail.com>
2023-07-18 22:06:03 +0530

    Adding filter input

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const newItemList = props.expenses.map((expenses, index) => (
     <ExpenseItem
       key={index}
@@ -13,7 +21,15 @@ const Expenses = (props) => {
     />
   ));
 
-  return <Card className="expenses">{newItemList}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {newItemList}
+    </Card>
+  );
 };
 
 export default Expenses;
